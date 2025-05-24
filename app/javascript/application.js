0c8cf40f87172ca9315c1e2ae197b56136946659
Shashank Jallapelli <shashankjallapelli@gmail.com>
2025-05-24 13:35:12 +0530

    Initial Setup

---
+++ app/javascript/application.js
@@ -0,0 +1,5 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+console.log("Running rails 7.2.0 with vite_rails gem!")
